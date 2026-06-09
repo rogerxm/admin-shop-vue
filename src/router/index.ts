993064ad4a987619f9c48b6476eb3fad34a0a23a
Roger Montes <rogerxmontes@gmail.com>
2026-06-09 12:26:15 -0600

    add router configuration with ShopLayout and HomeView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,22 @@
+import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'shop',
+      component: ShopLayout,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: () => import('@/modules/shop/views/HomeView.vue'),
+        },
+      ],
+    },
+  ],
 });
 
 export default router;
