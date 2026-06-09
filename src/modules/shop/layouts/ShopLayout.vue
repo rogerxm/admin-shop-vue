e6302b2fad0bc18af53971fdf38326f1af44248b
Roger Montes <rogerxmontes@gmail.com>
2026-06-09 12:26:03 -0600

    add ShopLayout component with TopMenu and CustomFooter

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <TopMenu />
+
+  <!-- Router View -->
+  <RouterView />
+
+  <CustomFooter />
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
