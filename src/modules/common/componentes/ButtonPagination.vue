620a24a91842eb0956ef816c1dcd7cc0e7e5a25f
Roger Montes <rogerxmontes@gmail.com>
2026-06-11 22:06:13 -0600

    add product listing and pagination components with loading state

---
+++ src/modules/common/componentes/ButtonPagination.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center py-10 bg-gray-100 space-x-3">
+    <button
+      class="flex items-center space-x-1.5 rounded-lg px-4 py-1.5 text-white bg-blue-500 disabled:bg-gray-300 hover:bg-blue-400 transition-all cursor-pointer">
+      <span>Anteriores</span>
+    </button>
+
+    <button
+      class="flex items-center space-x-1.5 rounded-lg px-4 py-1.5 text-white bg-blue-500 disabled:bg-gray-300 hover:bg-blue-400 transition-all cursor-pointer">
+      <span>Siguientes</span>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/modules/products/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="py-10 bg-gray-100">
+    <div class="mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <ProductCard v-for="product in products" :product="product" :key="product.id" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import ProductCard from './ProductCard.vue';
+import type { Product } from '../interfaces/product.interface';
+
+const props = defineProps<({
+  products: Product[]
+})>()
+</script>
