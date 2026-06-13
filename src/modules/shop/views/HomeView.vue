c8753fe81d51e4ac0a6d2a810b24f9d21dfc3cf1
Roger Montes <rogerxmontes@gmail.com>
2026-06-13 11:55:57 -0600

    refactor HomeView to enhance product pagination and loading state handling

--- src/modules/shop/views/HomeView.vue
+++ src/modules/shop/views/HomeView.vue
@@ -50,19 +50,40 @@
   <ProductList v-else :products="products" />
 
 
-  <ButtonPagination />
+  <ButtonPagination :page="page" :has-more-data="!!products && products.length < 10" />
 </template>
 
 <script lang="ts" setup>
 import ButtonPagination from '@/modules/common/componentes/ButtonPagination.vue';
 import { getProductsAction } from '@/modules/products/actions';
 import ProductList from '@/modules/products/components/ProductList.vue';
-import { useQuery } from '@tanstack/vue-query';
+import { useQuery, useQueryClient } from '@tanstack/vue-query';
+import { ref, watch, watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute()
+const page = ref(Number(route.query.page || 1))
+const queryClient = useQueryClient()
 
 const { data: products = [] } = useQuery({
-  queryKey: ['products', { page: 1 }],
-  queryFn: () => getProductsAction(),
+  queryKey: ['products', { page }],
+  queryFn: () => getProductsAction(page.value),
+})
+
+watch(
+  () => route.query.page,
+  (newPage) => {
+    page.value = Number(newPage || 1)
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+)
+
+watchEffect(() => {
+  queryClient.prefetchQuery({
+    queryKey: ['products', { page: page.value + 1 }],
+    queryFn: () => getProductsAction(page.value + 1),
+  })
 })
 
 </script>
