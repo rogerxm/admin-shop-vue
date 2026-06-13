25836031bf54127683381c146ca0fce0e13b3037
Roger Montes <rogerxmontes@gmail.com>
2026-06-13 11:55:47 -0600

    refactor ButtonPagination component to improve button attributes and prop definitions

--- src/modules/common/componentes/ButtonPagination.vue
+++ src/modules/common/componentes/ButtonPagination.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex justify-center py-10 bg-gray-100 space-x-3">
-    <button
+    <button :disabled="page === 1" @click="$router.push({ query: { page: page - 1 } })"
       class="flex items-center space-x-1.5 rounded-lg px-4 py-1.5 text-white bg-blue-500 disabled:bg-gray-300 hover:bg-blue-400 transition-all cursor-pointer">
       <span>Anteriores</span>
     </button>
 
-    <button
+    <button :disabled="hasMoreData" @click="$router.push({ query: { page: page + 1 } })"
       class="flex items-center space-x-1.5 rounded-lg px-4 py-1.5 text-white bg-blue-500 disabled:bg-gray-300 hover:bg-blue-400 transition-all cursor-pointer">
       <span>Siguientes</span>
     </button>
@@ -13,4 +13,11 @@
 </template>
 
 <script setup lang="ts">
+interface Props {
+  page: number,
+  hasMoreData: boolean
+}
+
+defineProps<Props>()
+
 </script>
