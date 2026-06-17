75ea3ef4b0842b003cc7bec24a99bb2857116dbd
Roger Montes <rogerxmontes@gmail.com>
2026-06-16 23:02:30 -0600

    refactor LoginView to use reactive form and update login handling

--- src/modules/auth/views/LoginView.vue
+++ src/modules/auth/views/LoginView.vue
@@ -1,31 +1,23 @@
 <template>
   <h1 class="text-2xl font-semibold mb-4">Login</h1>
-  <form action="#" method="POST">
-    <!-- Username Input -->
+  <form @submit.prevent="onLogin">
+    <!-- Email Input -->
     <div class="mb-4">
-      <label for="username" class="block text-gray-600">Username</label>
-      <input
-        type="text"
-        id="username"
-        name="username"
+      <label for="username" class="block text-gray-600">Email</label>
+      <input v-model="form.email" type="text" id="email" name="email"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-        autocomplete="off"
-      />
+        autocomplete="off" />
     </div>
     <!-- Password Input -->
     <div class="mb-4">
       <label for="password" class="block text-gray-600">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
+      <input v-model="form.password" type="password" id="password" name="password"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-        autocomplete="off"
-      />
+        autocomplete="off" />
     </div>
     <!-- Remember Me Checkbox -->
     <div class="mb-4 flex items-center">
-      <input type="checkbox" id="remember" name="remember" class="text-blue-500" />
+      <input v-model="form.rememberMe" type="checkbox" id="remember" name="remember" class="text-blue-500" />
       <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
     </div>
     <!-- Forgot Password Link -->
@@ -33,11 +25,7 @@
       <a href="#" class="hover:underline">Forgot Password?</a>
     </div>
     <!-- Login Button -->
-    <button
-      @click="onLogin"
-      type="button"
-      class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
-    >
+    <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">
       Login
     </button>
   </form>
@@ -48,18 +36,21 @@
 </template>
 
 <script lang="ts" setup>
+import { useAuthStore } from '@/stores/auth.store';
+import { reactive } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
+const authStore = useAuthStore()
 
-const onLogin = () => {
-  localStorage.setItem('userId', 'ABC-123');
+const form = reactive({
+  email: '',
+  password: '',
+  rememberMe: false
+})
 
-  const lastPath = localStorage.getItem('lastPath') ?? '/';
-
-  // router.replace({
-  //   // name: 'home',
-  // });
-  router.replace(lastPath);
+const onLogin = async () => {
+  const response = await authStore.login(form.email, form.password)
+  console.log(response)
 };
 </script>
