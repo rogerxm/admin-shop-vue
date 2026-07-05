edcf059db5b72f2f25bc8c3384520487e718caef
Roger Montes <rogerxmontes@gmail.com>
2026-07-05 13:41:54 -0600

    refactor LoginView to improve form handling and enhance user experience

--- src/modules/auth/views/LoginView.vue
+++ src/modules/auth/views/LoginView.vue
@@ -3,21 +3,39 @@
   <form @submit.prevent="onLogin">
     <!-- Email Input -->
     <div class="mb-4">
-      <label for="username" class="block text-gray-600">Email</label>
-      <input v-model="form.email" type="text" id="email" name="email"
+      <label for="email" class="block text-gray-600">Email</label>
+      <input
+        ref="emailInputRef"
+        v-model="form.email"
+        type="text"
+        id="email"
+        name="email"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-        autocomplete="off" />
+        autocomplete="off"
+      />
     </div>
     <!-- Password Input -->
     <div class="mb-4">
       <label for="password" class="block text-gray-600">Password</label>
-      <input v-model="form.password" type="password" id="password" name="password"
+      <input
+        ref="passwordInputRef"
+        v-model="form.password"
+        type="password"
+        id="password"
+        name="password"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-        autocomplete="off" />
+        autocomplete="off"
+      />
     </div>
     <!-- Remember Me Checkbox -->
     <div class="mb-4 flex items-center">
-      <input v-model="form.rememberMe" type="checkbox" id="remember" name="remember" class="text-blue-500" />
+      <input
+        v-model="form.rememberMe"
+        type="checkbox"
+        id="remember"
+        name="remember"
+        class="text-blue-500"
+      />
       <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
     </div>
     <!-- Forgot Password Link -->
@@ -25,7 +43,10 @@
       <a href="#" class="hover:underline">Forgot Password?</a>
     </div>
     <!-- Login Button -->
-    <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">
+    <button
+      type="submit"
+      class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+    >
       Login
     </button>
   </form>
@@ -37,20 +58,46 @@
 
 <script lang="ts" setup>
 import { useAuthStore } from '@/stores/auth.store';
-import { reactive } from 'vue';
-import { useRouter } from 'vue-router';
+import { reactive, ref, watchEffect } from 'vue';
+import { useToast } from 'vue-toastification';
 
-const router = useRouter();
-const authStore = useAuthStore()
+const authStore = useAuthStore();
+const emailInputRef = ref<HTMLInputElement | null>(null);
+const passwordInputRef = ref<HTMLInputElement | null>(null);
+const toast = useToast();
 
 const form = reactive({
   email: '',
   password: '',
-  rememberMe: false
-})
+  rememberMe: false,
+});
 
 const onLogin = async () => {
-  const response = await authStore.login(form.email, form.password)
-  console.log(response)
+  if (!form.email) {
+    return emailInputRef.value?.focus();
+  }
+
+  if (!form.password) {
+    return passwordInputRef.value?.focus();
+  }
+
+  if (form.rememberMe) {
+    localStorage.setItem('email', form.email);
+  } else {
+    localStorage.removeItem('email');
+  }
+
+  const response = await authStore.login(form.email, form.password);
+  if (response) return;
+
+  toast.error('Invalid email or password');
 };
+
+watchEffect(() => {
+  const email = localStorage.getItem('email');
+  if (email) {
+    form.email = email;
+    form.rememberMe = true;
+  }
+});
 </script>
