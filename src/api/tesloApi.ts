45d6965f767f43bafc06ca74a19574f676e35733
Roger Montes <rogerxmontes@gmail.com>
2026-07-06 22:04:57 -0600

    add request interceptor to include authorization token in headers

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -5,5 +5,14 @@ const tesloApi = axios.create({
 });
 
 // Interceptors
+tesloApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 export { tesloApi };
