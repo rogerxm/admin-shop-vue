5a023f49c089590004785720209de72d8a6132e2
Roger Montes <rogerxmontes@gmail.com>
2026-07-06 22:19:14 -0600

    add auth store subscription to manage authentication state and routing

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,30 @@
 </template>
 
 <script setup lang="ts">
-import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+import { useAuthStore } from './stores/auth.store';
+import { useRoute, useRouter } from 'vue-router';
+import { AuthStatus } from './modules/auth/interfaces';
+
+const authStore = useAuthStore();
+
+const router = useRouter();
+const route = useRoute();
+
+authStore.$subscribe(
+  (_, state) => {
+    if (state.authStatus === AuthStatus.Checking) {
+      authStore.checkAuthStatus();
+
+      return;
+    }
+
+    if (route.path.includes('/auth') && state.authStatus === AuthStatus.Authenticated) {
+      router.replace({ name: 'home' });
+
+      return;
+    }
+  },
+  { immediate: true },
+);
 </script>
