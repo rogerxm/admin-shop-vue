e8b9bae621dae7df15c2d6b05f9afb943994a8ab
Roger Montes <rogerxmontes@gmail.com>
2026-07-11 12:03:02 -0600

    feat: implement AdminLayout and enhance TopMenu with authentication-based navigation

--- src/modules/shop/components/TopMenu.vue
+++ src/modules/shop/components/TopMenu.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- Header Navbar -->
   <nav class="fixed top-0 left-0 z-20 w-full border-b border-gray-200 bg-white py-2.5 px-6 sm:px-4">
     <div class="container mx-auto flex max-w-6xl flex-wrap items-center justify-between">
       <a href="#" class="flex items-center">
@@ -21,23 +20,46 @@
         <span class="self-center whitespace-nowrap text-xl font-semibold">Termcode</span>
       </a>
       <div class="mt-2 sm:mt-0 sm:flex md:order-2">
-        <!-- Login Button -->
-        <RouterLink
-          to="/auth/login"
-          type="button"
-          class="rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg"
-        >
-          Login
-        </RouterLink>
-        <button
-          type="button"
-          class="rounde mr-3 hidden bg-blue-700 py-1.5 px-6 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:mr-0 md:inline-block rounded-lg"
-        >
-          Register
-        </button>
+        <template v-if="!authStore.isAuthenticated">
+          <!-- Login Button -->
+          <RouterLink
+            to="/auth/login"
+            type="button"
+            class="mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg"
+          >
+            Login
+          </RouterLink>
+          <RouterLink
+            to="/auth/register"
+            type="button"
+            class="mr-3 hidden bg-blue-700 py-1.5 px-6 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:mr-0 md:inline-block rounded-lg"
+          >
+            Register
+          </RouterLink>
+        </template>
+
+        <template v-if="authStore.isAuthenticated">
+          <!-- Login Button -->
+          <RouterLink
+            v-if="authStore.isAdmin"
+            to="/admin"
+            type="button"
+            class="mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg"
+          >
+            Admin
+          </RouterLink>
+          <button
+            @click="authStore.logout()"
+            type="button"
+            class="mr-3 hidden bg-blue-700 py-1.5 px-6 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:mr-0 md:inline-block rounded-lg"
+          >
+            Cerrar sesión
+          </button>
+        </template>
+
         <!-- Register Button -->
-        <RouterLink
-          to="/auth/register"
+        <button
+          data-collapse-toggle="navbar-sticky"
           type="button"
           class="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
           aria-controls="navbar-sticky"
@@ -57,7 +79,7 @@
               clip-rule="evenodd"
             ></path>
           </svg>
-        </RouterLink>
+        </button>
       </div>
       <div
         class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
@@ -100,3 +122,9 @@
     </div>
   </nav>
 </template>
+
+<script lang="ts" setup>
+import { useAuthStore } from '@/stores/auth.store';
+
+const authStore = useAuthStore();
+</script>
