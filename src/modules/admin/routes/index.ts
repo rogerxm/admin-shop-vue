25abe239e93afad0e6962bcd61c49cd8bac8f6e3
Roger Montes <rogerxmontes@gmail.com>
2026-07-11 13:28:56 -0600

    feat: add admin routes for dashboard and products views

--- src/modules/admin/routes/index.ts
+++ src/modules/admin/routes/index.ts
@@ -1,11 +1,23 @@
 import isAdminGuard from '@/modules/auth/guards/is-admin.guard';
 import isAuthenticatedGuard from '@/modules/auth/guards/is-authenticated.guard';
-import isNotAuthenticatedGuard from '@/modules/auth/guards/is-not-authenticated.guard';
 import type { RouteRecordRaw } from 'vue-router';
 
 export const adminRoutes: RouteRecordRaw = {
   path: '/admin',
   name: 'admin',
   beforeEnter: [isAuthenticatedGuard, isAdminGuard],
+  redirect: { name: 'admin-dashboard' },
   component: () => import('@/modules/admin/layouts/AdminLayout.vue'),
+  children: [
+    {
+      path: 'dashboard',
+      name: 'admin-dashboard',
+      component: () => import('@/modules/admin/views/DashboardView.vue'),
+    },
+    {
+      path: 'products',
+      name: 'admin-products',
+      component: () => import('@/modules/admin/views/ProductsView.vue'),
+    },
+  ],
 };
